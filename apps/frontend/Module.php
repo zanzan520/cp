1b38663b0d71132a3f0d76f9d81c3507c45cec2a
zanzan <732601408@qq.com>
2017-03-24 14:04:09 +0800

    add

---
+++ apps/frontend/Module.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Website\Frontend;
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\Mvc\ModuleDefinitionInterface;
+
+
+class Module implements ModuleDefinitionInterface
+{
+
+    /**
+     * Registers the module auto-loader
+     */
+    public function registerAutoloaders()
+    {
+
+        $loader = new Loader();
+
+        $loader->registerNamespaces(array(
+            'Website\Frontend\Controllers' => __DIR__ . '/controllers/',
+            'Website\Frontend\Models' => __DIR__ . '/models/',
+        ));
+
+        $loader->register();
+    }
+
+    /**
+     * Registers the module-only services
+     *
+     * @param Phalcon\DI $di
+     */
+    public function registerServices($di)
+    {
+
+        /**
+         * Read configuration
+         */
+        $config = include __DIR__ . "/config/config.php";
+
+        /**
+         * Setting up the view component
+         */
+        $di['view'] = function () {
+            $view = new View();
+            $view->setViewsDir(__DIR__ . '/views/');
+
+            return $view;
+        };
+
+        /**
+         * Database connection is created based in the parameters defined in the configuration file
+         */
+        $di['db'] = function () use ($config) {
+            return new DbAdapter($config->toArray());
+        };
+
+    }
+
+}
---
+++ apps/frontend/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Website\Frontend\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
